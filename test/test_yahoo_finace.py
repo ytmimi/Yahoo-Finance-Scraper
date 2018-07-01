d00bf1ca9a2aec99036977c097557ba8fadc101e
Yacin Tmimi <yacintmimi@gmail.com>
2018-07-01 19:06:14 -0400

    split tests into those that test the url processing and those that work with the requested data

--- test/test_yahoo_finace.py
+++ test/test_yahoo_finace.py
@@ -4,58 +4,97 @@ import sys
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(path, 'yahoo_finance'))
 
-
-from yahoo_finance import Stock_Data, UTC_Converter
+from yahoo_finance import Stock_Data
 from collections import OrderedDict
 import pandas as pd
 import datetime as dt
 import unittest
+from unittest.mock import patch
 
 
 class Test_Data_Request(unittest.TestCase):
+	#mocking to prevent a get request
+	@classmethod
+	@patch.object(Stock_Data, 'request_data')
+	def setUpClass(cls, mock_request):
+		mock_request.return_value = 'some response'
+		cls.ticker = 'AMD'
+		cls.stock_data = Stock_Data(cls.ticker)
+		mock_request.assert_called()
+		
+	def expect_url(self, data_obj):
+		utc = data_obj.utc
+		base_url = data_obj.url.replace('_', data_obj.ticker)
+		append_url = data_obj.url_append.replace('start', f'{utc(data_obj.start)}'
+			).replace('end', f'{utc(data_obj.end)}').replace('rate', data_obj.frequency)
+		return base_url.replace(f'p={data_obj.ticker}', append_url)
+
+
+	def test_process_url_default(self):
+		result = self.stock_data.process_url(self.stock_data.url)
+		expected = self.stock_data.url.replace('_', self.stock_data.ticker)
+		self.assertEqual(result, expected)
+
+
+	def test_process_url_custom_start(self):
+		start='07/01/2018'
+		data = Stock_Data(self.ticker, start=start)
+		result = data.process_url(data.url)
+		expected = self.expect_url(data)
+		self.assertEqual(result, expected)
+		self.assertIn(f'{data.utc(data.start)}', expected)
+
+
+	def test_process_url_custom_end(self):
+		end='07/01/2016'
+		data = Stock_Data(self.ticker, end=end)
+		result = data.process_url(data.url)
+		expected = self.expect_url(data)
+		self.assertEqual(result, expected)
+		self.assertIn(f'{data.utc(data.end)}', expected)
+
+
+	def test_process_url_custom_frequency(self):
+		frequency = 'mo'
+		data = Stock_Data(self.ticker, frequency=frequency)
+		result = data.process_url(data.url)
+		expected = self.expect_url(data)
+		self.assertEqual(result, expected)
+		self.assertIn(f'{data.frequency}', expected)
+
+
+@unittest.skip('Only run when testing full functionality')
+class Test_Request_Data_Handling(unittest.TestCase):
 	@classmethod
 	def setUpClass(cls):
 		cls.ticker = 'AMD'
 		cls.stock_data = Stock_Data(cls.ticker)
-		cls.response = cls.stock_data.request_data()
 
+	def test_good_request(self):
+		self.assertEqual(self.stock_data.response.status_code, 200)
 	
-	@unittest.skip('Testing other')
-	def test_good_request_yahoo_finance(self):
-		self.assertEqual(self.response.status_code, 200)
-
-	@unittest.skip('Need to figure this out')
-	def test_bad_request_yahoo_finance(self):
-		pass
-
-	@unittest.skip('Testing other')
 	def test_get_data_table(self):
 		table = self.stock_data.get_data_table()
 		self.assertIn('<table', table.html )
-
-	@unittest.skip('Testing other')
+	
 	def test_table_head(self):
 		table = self.stock_data.get_data_table()
 		head = self.stock_data.table_head(table)
 		self.assertIn('<thead', head.html)
-
-	@unittest.skip('Testing other')
+	
 	def test_table_body(self):
 		table = self.stock_data.get_data_table()
 		body = self.stock_data.table_body(table)
 		self.assertIn('<tbody', body.html)
-
-	@unittest.skip('Testing other')
+	
 	def test_column_headers(self):
 		headers = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
 		self.assertEqual(headers, self.stock_data.column_headers())
-
-	@unittest.skip('Testing other')
+	
 	def test_table_to_dict(self):
 		table_dict = self.stock_data.table_to_dict()
 		self.assertIsInstance(table_dict, OrderedDict,)
 
-	@unittest.skip('Testing other')
 	def test_data_frame(self):
 		df = self.stock_data.data_frame()
 		self.assertIsInstance(df , pd.DataFrame,)
@@ -63,44 +102,6 @@ class Test_Data_Request(unittest.TestCase):
 		self.assertTrue(df['Date'].iloc[0] <= dt.date.today())
 		#if last day is a weekend or non trading day, the closest subsequent trading day is last
 		self.assertTrue(df['Date'].iloc[-1] >= dt.date.today() - dt.timedelta(days=365))
-
-	
-class Test_Time_Conversion(unittest.TestCase):
-	def test_utc_string_right_fmt(self):
-		# 1530331200 = 06/30/2018 @ 4:00am (UTC)
-		# 1498795200 = 06/30/2017 @ 4:00am (UTC)
-		utc = UTC_Converter('06/30/2018').__call__(fmt_str='%m/%d/%Y')
-		self.assertEqual(utc, 1530331200)
-
-	def test_utc_string_right_fmt(self):
-		with self.assertRaises(ValueError) as ve:
-			utc = UTC_Converter('06/30/2018').__call__(fmt_str='%Y/%m/%d')
-			self.assertEqual(ve.msg, 'The fmt_str did not match the date provided.')
-
-	def test_utc_string_default(self):
-		utc = UTC_Converter('06/30/2018').__call__()
-		self.assertEqual(utc, 1530331200)
-
-	def test_utc_string_default_error(self):
-		with self.assertRaises(ValueError) as ve:
-			utc = UTC_Converter('June 30, 2018').__call__()
-			self.assertEqual(ve.msg, 'Unable to parse string. Please supply the fmt_str kwarg.')
-
-	def test_utc_from_datetime(self):
-		# 1498795200 = 06/30/2017 @ 4:00am (UTC)
-		date = dt.datetime.strptime('06/30/2017','%m/%d/%Y')
-		utc = UTC_Converter(date).__call__()
-		self.assertEqual(utc, 1498795200)
-
-	def test_utc_from_date(self):
-		# 1498795200 = 06/30/2017 @ 4:00am (UTC)
-		date = dt.datetime.strptime('06/30/2017','%m/%d/%Y').date()
-		utc = UTC_Converter(date).__call__()
-		self.assertEqual(utc, 1498795200)
-
-
-
-		
 
 
 if __name__ == '__main__':
