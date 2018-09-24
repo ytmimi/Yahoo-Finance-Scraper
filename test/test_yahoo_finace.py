44639d6094820a47bdaa330c65b4baa5e1e2d3fe
Yacin Tmimi <yacintmimi@gmail.com>
2018-09-24 14:48:06 -0400

    rewrote the entire scrapper and tests. Now includes class that handle data from various endpoints

--- test/test_yahoo_finace.py
+++ test/test_yahoo_finace.py
@@ -1,134 +1,228 @@
 import os
 import sys
+import datetime as dt
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(path, 'yahoo_finance'))
+from yahoo_finance import (Base_Scraper, Stock_Scraper, Statistics_Scraper,Profile_Scraper,
+	Income_Statement_Scraper, Balance_Sheet_Scraper, Cash_Flow_Scraper,
+	Analysis_Scraper, Options_Scraper, Holders_Scraper, Sustainability_Scraper,)
 
-from yahoo_finance import Stock_Data
-from collections import OrderedDict
-import pandas as pd
-import datetime as dt
-import unittest
 from unittest.mock import patch
+import pytest
+
+LIVE_TEST = False
 
 
-class Test_Data_Request(unittest.TestCase):
-	#mocking to prevent a get request
-	@classmethod
-	@patch.object(Stock_Data, 'request_data')
-	def setUpClass(cls, mock_request):
-		mock_request.return_value = 'some response'
-		cls.ticker = 'AMD'
-		cls.stock_data = Stock_Data(cls.ticker)
-		mock_request.assert_called()
+class Test_Base_Scraper:
+	def test_base_url(self):
+		assert Base_Scraper('IBM').base_url == 'https://finance.yahoo.com/quote/IBM'
+
+	def test_default_query_string(self):
+		assert Base_Scraper('IBM')._default_query_string() == 'p=IBM'
+
+	def test_url_base_qs(self):
+		assert Base_Scraper('IBM')._process_url() == (
+			'https://finance.yahoo.com/quote/IBM?p=IBM'
+			)
+
+class Test_Stock_Scraper:
+	def test_base_url(self):
+		scraper =  Stock_Scraper('IBM')
+		assert scraper.base_url == 'https://finance.yahoo.com/quote/IBM/history'
+
+	def test_advanced_qs(self):
+		ticker = 'AAPL'
+		start = '10/20/2013'
+		end = '10/20/2014'
+		freq = '1d'
+		scraper = Stock_Scraper(ticker, start, end, freq)
+		assert scraper._advanced_query_string() == (
+			'period1=1382241600&period2=1413777600&interval=1d&filter=history&frequency=1d'
+			)
+
+	def test_url_base_qs(self):
+		scraper = Stock_Scraper('AAPL')
+		scraper._process_url() == 'https://finance.yahoo.com/quote/AAPL/history?p=AAPL'
 		
-	def expect_url(self, data_obj):
-		utc = data_obj.utc
-		base_url = data_obj.url.replace('_', data_obj.ticker)
-		append_url = data_obj.url_append.replace('start', f'{utc(data_obj.start)}'
-			).replace('end', f'{utc(data_obj.end)}').replace('rate', data_obj.frequency)
-		return base_url.replace(f'p={data_obj.ticker}', append_url)
+	def test_url_advanced_qs(self):
+		ticker = 'AAPL'
+		start = '10/20/2013'
+		end = '10/20/2014'
+		freq = '1d'
+		scraper = Stock_Scraper(ticker, start, end, freq)
+		assert scraper._process_url() == (
+			'https://finance.yahoo.com/quote/AAPL/history?'
+			'period1=1382241600&period2=1413777600&interval=1d&filter=history&frequency=1d'
+			)
 
+class Test_Statistics_Scraper:
+	def test_base_url(self):
+		assert Statistics_Scraper('NFLX').base_url == (
+			'https://finance.yahoo.com/quote/NFLX/key-statistics'
+			)
 
-	def test_process_url_default(self):
-		result = self.stock_data.process_url(self.stock_data.url)
-		expected = self.stock_data.url.replace('_', self.stock_data.ticker)
-		self.assertEqual(result, expected)
+	def test_url_base_qs(self):
+		assert Statistics_Scraper('NFLX')._process_url() == (
+			'https://finance.yahoo.com/quote/NFLX/key-statistics?p=NFLX'
+			)
 
+class Test_Profile_Scraper:
+	def test_base_url(self):
+		assert Profile_Scraper('NFLX').base_url == (
+			'https://finance.yahoo.com/quote/NFLX/profile'
+			)
 
-	def test_process_url_custom_start(self):
-		start='07/01/2018'
-		data = Stock_Data(self.ticker, start=start)
-		result = data.process_url(data.url)
-		expected = self.expect_url(data)
-		self.assertEqual(result, expected)
-		self.assertIn(f'{data.utc(data.start)}', expected)
+	def test_url_base_qs(self):
+		assert Profile_Scraper('NFLX')._process_url() == (
+			'https://finance.yahoo.com/quote/NFLX/profile?p=NFLX'
+			)
 
+class Test_Income_Statement_Scraper:
+	def test_base_url(self):
+		assert Income_Statement_Scraper('NFLX').base_url == (
+			'https://finance.yahoo.com/quote/NFLX/financials'
+			)
 
-	def test_process_url_custom_end(self):
-		end='07/01/2016'
-		data = Stock_Data(self.ticker, end=end)
-		result = data.process_url(data.url)
-		expected = self.expect_url(data)
-		self.assertEqual(result, expected)
-		self.assertIn(f'{data.utc(data.end)}', expected)
+	def test_url_base_qs(self):
+		assert Income_Statement_Scraper('NFLX')._process_url() == (
+			'https://finance.yahoo.com/quote/NFLX/financials?p=NFLX'
+			)
 
+class Test_Balance_Sheet_Scraper:
+	def test_base_url(self):
+		assert Balance_Sheet_Scraper('NFLX').base_url == (
+			'https://finance.yahoo.com/quote/NFLX/balance-sheet'
+			)
 
-	def test_process_url_custom_frequency(self):
-		frequency = 'mo'
-		data = Stock_Data(self.ticker, frequency=frequency)
-		result = data.process_url(data.url)
-		expected = self.expect_url(data)
-		self.assertEqual(result, expected)
-		self.assertIn(f'{data.frequency}', expected)
+	def test_url_base_qs(self):
+		assert Balance_Sheet_Scraper('NFLX')._process_url() == (
+			'https://finance.yahoo.com/quote/NFLX/balance-sheet?p=NFLX'
+			)
 
+class Test_Cash_Flow_Scraper:
+	def test_base_url(self):
+		assert Cash_Flow_Scraper('NFLX').base_url == (
+			'https://finance.yahoo.com/quote/NFLX/cash-flow'
+			)
 
-@unittest.skip('Only run when testing full functionality')
-class Test_Request_Data_Handling(unittest.TestCase):
-	@classmethod
-	def setUpClass(cls):
-		cls.ticker = 'AMD'
-		cls.stock_data = Stock_Data(cls.ticker)
+	def test_url_base_qs(self):
+		assert Cash_Flow_Scraper('NFLX')._process_url() == (
+			'https://finance.yahoo.com/quote/NFLX/cash-flow?p=NFLX'
+			)
 
-	def test_good_request(self):
-		self.assertEqual(self.stock_data.response.status_code, 200)
+class Test_Analysis_Scraper:
+	def test_base_url(self):
+		assert Analysis_Scraper('NFLX').base_url == (
+			'https://finance.yahoo.com/quote/NFLX/analysis'
+			)
+
+	def test_url_base_qs(self):
+		assert Analysis_Scraper('NFLX')._process_url() == (
+			'https://finance.yahoo.com/quote/NFLX/analysis?p=NFLX'
+			)
+
+class Test_Options_Scraper:
+	def test_base_url(self):
+		assert Options_Scraper('NFLX').base_url == (
+			'https://finance.yahoo.com/quote/NFLX/options'
+			)
+
+	def test_url_base_qs(self):
+		assert Options_Scraper('NFLX')._process_url() == (
+			'https://finance.yahoo.com/quote/NFLX/options?p=NFLX'
+			)
 	
-	def test_get_data_table(self):
-		table = self.stock_data.get_data_table()
-		self.assertIn('<table', table.html )
-	
-	def test_table_head(self):
-		table = self.stock_data.get_data_table()
-		head = self.stock_data.table_head(table)
-		self.assertIn('<thead', head.html)
-	
-	def test_table_body(self):
-		table = self.stock_data.get_data_table()
-		body = self.stock_data.table_body(table)
-		self.assertIn('<tbody', body.html)
-	
-	def test_column_headers(self):
-		headers = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
-		self.assertEqual(headers, self.stock_data.column_headers())
-	
-	def test_table_to_dict(self):
-		table_dict = self.stock_data.table_to_dict()
-		self.assertIsInstance(table_dict, OrderedDict,)
+	def test_url_advanced_qs(self):
+		assert Options_Scraper('NFLX', '10/12/2018')._process_url() == (
+			'https://finance.yahoo.com/quote/NFLX/options?p=NFLX&date=1539302400'
+			)
 
-	def test_data_frame(self):
-		df = self.stock_data.data_frame()
-		self.assertIsInstance(df , pd.DataFrame,)
-		#if today is a weekend or non trading day, the most recent trading day is first
-		self.assertTrue(df['Date'].iloc[0] <= dt.date.today())
-		#if last day is a weekend or non trading day, the closest subsequent trading day is last
-		self.assertTrue(df['Date'].iloc[-1] >= dt.date.today() - dt.timedelta(days=365))
+class Test_Holders_Scraper:
+	def test_base_url(self):
+		assert Holders_Scraper('NFLX').base_url == (
+			'https://finance.yahoo.com/quote/NFLX/holders'
+			)
+
+	def test_url_base_qs(self):
+		assert Holders_Scraper('NFLX')._process_url() == (
+			'https://finance.yahoo.com/quote/NFLX/holders?p=NFLX'
+			)
+
+class Test_Sustainability_Scraper:
+	def test_base_url(self):
+		assert Sustainability_Scraper('NFLX').base_url == (
+			'https://finance.yahoo.com/quote/NFLX/sustainability'
+			)
+
+	def test_url_base_qs(self):
+		assert Sustainability_Scraper('NFLX')._process_url() == (
+			'https://finance.yahoo.com/quote/NFLX/sustainability?p=NFLX'
+			)
 
 
-class Test_Request_Data_Handling_Custom_Inputs(unittest.TestCase):
-	@classmethod
-	def setUpClass(cls):
-		cls.ticker = 'AMD'
-		cls.start = '02/02/2017'
-		cls.end = '02/02/2018'
-		cls.frequency = 'mo'
-		cls.stock_data = Stock_Data(cls.ticker, start=cls.start,
-						end=cls.end, frequency=cls.frequency)
 
-	def test_data_frame_custom_date_range(self):
-		df = self.stock_data.data_frame()
-		self.assertIsInstance(df , pd.DataFrame,)
-		first = dt.datetime.strptime(self.end, '%m/%d/%Y').date()
-		last = dt.datetime.strptime(self.start, '%m/%d/%Y').date()
-		#test that the data in the dataframe corresponds with the custom start and end
-		self.assertTrue(df['Date'].iloc[0].year == first.year )
-		self.assertTrue(df['Date'].iloc[0].month == first.month )
-		self.assertTrue(df['Date'].iloc[-1].year == last.year)
-		self.assertTrue(df['Date'].iloc[-1].month == last.month)
+
+@pytest.mark.skipif(not LIVE_TEST, reason='These tests each make seperate request to yahoo finance')
+class Test_Requests:
+	def test_base(self):
+		resp = Base_Scraper('JWN').request_data()
+		assert 'context' in resp.keys()
+
+	def test_base_stock(self):
+		resp = Stock_Scraper('JWN').request_data()
+		assert 'context' in resp.keys()
+
+	def test_advanced_stock(self):
+		resp = Stock_Scraper('JWN', '10/20/2013', 
+					'10/20/2014').request_data()
+		assert 'context' in resp.keys()
+
+	def test_statistics(self):
+		resp = Statistics_Scraper('NFLX').request_data()
+		assert 'context' in resp.keys()
+
+	def test_profile(self):
+		resp = Profile_Scraper('NFLX').request_data()
+		assert 'context' in resp.keys()
+
+	def test_income_statement(self):
+		resp = Income_Statement_Scraper('NFLX').request_data()
+		assert 'context' in resp.keys()
+
+	def test_balance_sheet(self):
+		resp = Balance_Sheet_Scraper('NFLX').request_data()
+		assert 'context' in resp.keys()
+
+	def test_cash_flow(self):
+		resp = Cash_Flow_Scraper('NFLX').request_data()
+		assert 'context' in resp.keys()
+
+	def test_analysis(self):
+		resp = Analysis_Scraper('MSFT').request_data()
+		assert 'context' in resp.keys()
+
+	def test_base_options(self):
+		resp = Options_Scraper('MSFT').request_data()
+		assert 'context' in resp.keys()
+
+	@pytest.mark.skip('fill out the body')
+	def test_advanced_options(self):
+		pass
+
+	def test_holders(self):
+		resp = Holders_Scraper('MSFT').request_data()
+		assert 'context' in resp.keys()
+
+	def test_sustainability(self):
+		resp = Sustainability_Scraper('MSFT').request_data()
+		assert 'context' in resp.keys()
+
 
 
 
 if __name__ == '__main__':
-	unittest.main()
+	pytest.main()
 
 
 
--- yahoo_finance/yahoo_finance.py
+++ yahoo_finance/yahoo_finance.py
@@ -1,101 +1,231 @@
-import pandas as pd
-import numpy as np
+import json
 import datetime as dt
+from datetime import date, timedelta
 from collections import OrderedDict
+
+from urllib.parse import urlencode
 from utc_converter import UTC_Converter
 from requests_html import HTMLSession
+import requests
+
+session = HTMLSession()
+utc = UTC_Converter()
 
 
-class Stock_Data():
-	url = 'https://finance.yahoo.com/quote/_/history?p=_'
-	url_append = 'period1=start&period2=end&interval=1rate&filter=history&frequency=1rate'
-	frequency_options = ['d', 'wk', 'mo']
-	session = HTMLSession()
-	utc = UTC_Converter()
-
-	def __init__(self, ticker, start=dt.date.today()+dt.timedelta(days=365), 
-				end=dt.date.today(), frequency='d', date_fmt_str=None):
-		self.ticker = ticker.upper()
-		self.start = start
-		self.end = end
-		self.frequency = frequency
-		self.date_fmt = date_fmt_str
-		self.response = self.request_data()
-
+class Base_Scraper:
+	_base_url = 'https://finance.yahoo.com/quote/_'
+	def __init__(self, ticker):
+		'''ticker: str, stock ticker'''
+		self.ticker = ticker
 
 	def request_data(self):
-		url = self.process_url(self.url)
-		return self.session.get(url)
+		'''
+		makes an HTTP GET request and returns json data
+		'''
+		resp = requests.get(self._process_url())
+		if resp.status_code < 400:
+			return self._process_response(resp)
+		return json.loads(resp.text)
+	
+	@property
+	def base_url(self):
+		'''includes the ticker in the base url'''
+		return self._base_url.replace('_', self.ticker)
 
-	def process_url(self, url):
-		url = self.url.replace('_', self.ticker)
-		default_start = dt.date.today()+dt.timedelta(days=365)
-		default_end = dt.date.today()
-		default_freq = 'd'
-		#if any of the default options are changed
-		if (self.start != default_start or self.end != default_end 
-			or self.frequency != default_freq):
-			start = self.utc(self.start, self.date_fmt)
-			end = self.utc(self.end, self.date_fmt) 		 
-			append = self.url_append.replace('start', f'{start}')
-			append = append.replace('end', f'{end}')
-			append = append.replace('rate', self.frequency)
-			url = url.replace(f'p={self.ticker}', append)
-			return url
+	def _process_url(self):
+		''' formats the url string'''
+		return f'{self.base_url}?{self._default_query_string()}'
 
-		else:
-			return url
+	def _default_query_string(self):
+		'''returns p=ticker'''
+		return urlencode({'p':self.ticker})
 
+	@staticmethod
+	def _process_response(resp):
+		'''
+		resp: request.Response object
 
-
-	def get_data_table(self):
-		# <table class="W(100%) M(0)" data-test="historical-prices">
-		table = self.response.html.find('table[data-test=historical-prices]', first=True)
-		return table
-
-	def table_head(self, table):
-		return table.find('thead',)[0]
-
-	def table_body(self, table):
-		return table.find('tbody',)[0]
-
-	def column_headers(self):
-		head = self.table_head(self.get_data_table())
-		cols = []
-		for item in head.find('tr>th'):
-			cols.append(item.text.replace('*',''))
-		return cols
-
-	def table_to_dict(self):
-		'''Convert html table into dictionary'''
-		headers = self.column_headers()
-		table = OrderedDict((head, []) for head in headers)
-		#searches the table body for tr tags
-		for tr in self.table_body(self.get_data_table()).find('tr'):
-			#searches for all td tags within each tr tag
-			for i, td in enumerate(tr.find('td')):
-				if headers[i] == 'Date':
-					value = dt.datetime.strptime(td.text, '%b %d, %Y').date()
-				else:
-					value = self.process_table_data(td.text)
-				table[headers[i]].append(value)
-		return table
-
-
-	def process_table_data(self, value):
-		if value == '-':
-			return np.nan
-		else:
-			return float(value.replace(',', ''))
-
-
-	def data_frame(self):
-		table = self.table_to_dict()
-		df = pd.DataFrame(table, index=table['Date'])
-		return df
+		when examining the response from Yahoo finance, I noticed that
+		I wasn't getting back HTML, but instead the html was being rendered with
+		JavaScript. Instead of trying to get the html, and then parsing that I think its more effective
+		to simply parse the text. This function parses the text from the response and returns a json
+		object with the data that would have been rendered to HTML  
+		'''
+		#after splitting and inspecting the string It was determined that the 44th index
+		#contained the information that we needed
+		block = resp.text.split('\n')[44]
+		#removes the unessessary "root.App.main = " and trailing ";" so that we are left with 
+		#a string representation of the json, and then we convert the json string into a python dict
+		return json.loads(block[16:-1])
 
 
 
+class Stock_Scraper(Base_Scraper):
+	endpoint = '/history'
+	frequency_options = ['1d', '1wk', '1mo']
+	
+	def __init__(self, ticker, start_date=date.today()-timedelta(days=365),
+		end_date=date.today(),frequency='1d', date_fmt_str=None):
+		'''
+		start_date: str, or datetime object
+		end_date: str or datetime object
+		frequency: str: '1d', '1wk', '1mo' for 1 day ,1 week, and 1 month respectively
+		date_fmt_str: standard datetime format string in case the string provided for 
+						start_date and end_date is unrecognized by the program.
+		'''
+		super().__init__(ticker)
+		self.start_date = start_date
+		self.end_date = end_date
+		self.frequency = self._validate_frequency(frequency)
+		self.date_fmt = date_fmt_str
+
+	@classmethod
+	def _validate_frequency(cls, frequency):
+		if frequency not in cls.frequency_options:
+			raise ValueError(f'frequency must be either {" or ".join(self.frequency_options)}.')
+		return frequency
+
+	@property
+	def base_url(self):
+		'''adds the endpoint to the base url'''
+		return super().base_url + self.endpoint
+
+	def _process_url(self):
+		if (self.end_date != date.today() or self.start_date != date.today()-timedelta(days=365)
+			or self.frequency != '1d'):
+			return f'{self.base_url}?{self._advanced_query_string()}'
+		return super()._process_url()
+
+	def _advanced_query_string(self):
+		url_params = {
+		'period1': utc(self.start_date, fmt_str=self.date_fmt),
+		'period2': utc(self.end_date, fmt_str=self.date_fmt),
+		'interval': self.frequency,
+		'filter':'history',
+		'frequency': self.frequency,
+		}
+		return urlencode(url_params)
+
+class Statistics_Scraper(Base_Scraper):
+	endpoint = '/key-statistics'
+	def __init__(self, ticker):
+		super().__init__(ticker)
+
+	@property
+	def base_url(self):
+		'''adds the endpoint to the base url'''
+		return super().base_url + self.endpoint
+
+class Profile_Scraper(Base_Scraper):
+	endpoint = '/profile'
+	def __init__(self, ticker):
+		super().__init__(ticker)
+
+	@property
+	def base_url(self):
+		'''adds the endpoint to the base url'''
+		return super().base_url + self.endpoint
+
+
+class Income_Statement_Scraper(Base_Scraper):
+	endpoint = '/financials'
+	def __init__(self, ticker):
+		super().__init__(ticker)
+
+	@property
+	def base_url(self):
+		'''adds the endpoint to the base url'''
+		return super().base_url + self.endpoint
+
+class Balance_Sheet_Scraper(Base_Scraper):
+	endpoint = '/balance-sheet'
+	def __init__(self, ticker):
+		super().__init__(ticker)
+
+	@property
+	def base_url(self):
+		'''adds the endpoint to the base url'''
+		return super().base_url + self.endpoint
+
+class Cash_Flow_Scraper(Base_Scraper):
+	endpoint = '/cash-flow'
+	def __init__(self, ticker):
+		super().__init__(ticker)
+
+	@property
+	def base_url(self):
+		'''adds the endpoint to the base url'''
+		return super().base_url + self.endpoint
+
+class Analysis_Scraper(Base_Scraper):
+	endpoint = '/analysis'
+	def __init__(self, ticker):
+		super().__init__(ticker)
+
+	@property
+	def base_url(self):
+		'''adds the endpoint to the base url'''
+		return super().base_url + self.endpoint
+
+class Options_Scraper(Base_Scraper):
+	endpoint = '/options'
+	def __init__(self, ticker, expiration_date=None, date_fmt=None):
+		'''
+		expiration_date: a str or datetime object
+		date_fmt: an appropriate date formate string if expiration_date is provided as a string
+		'''
+		super().__init__(ticker)
+		self.date = expiration_date
+		self.date_fmt = date_fmt
+
+	@property
+	def base_url(self):
+		'''adds the endpoint to the base url'''
+		return super().base_url + self.endpoint
+
+	def _process_url(self):
+		if (self.date):
+			return f'{self.base_url}?{self._advanced_query_string()}'
+		return super()._process_url()
+
+	def _advanced_query_string(self):
+		url_params = {
+		'p':self.ticker,
+		#offset by -4 hours to get to midnight
+		'date': utc(self.date, dt.timedelta(hours=-4), self.date_fmt),
+		}
+		return urlencode(url_params)
+
+class Holders_Scraper(Base_Scraper):
+	endpoint = '/holders'
+	def __init__(self, ticker):
+		super().__init__(ticker)
+
+	@property
+	def base_url(self):
+		'''adds the endpoint to the base url'''
+		return super().base_url + self.endpoint
+
+class Sustainability_Scraper(Base_Scraper):
+	endpoint = '/sustainability'
+	def __init__(self, ticker):
+		super().__init__(ticker)
+
+	@property
+	def base_url(self):
+		'''adds the endpoint to the base url'''
+		return super().base_url + self.endpoint
+
+
+
+if __name__ == '__main__':
+	
+	stock = Stock_Scraper('JWN', start_date='10/20/2014', end_date='10/30/2017', frequency='1wk').request_data()
+	options = Options_Scraper('JWN').request_data()
+	print(stock.keys())
+	print('\n')
+	print(options['context']['dispatcher']['stores']['OptionContractsStore'].keys())
+	
 
 
 
@@ -103,7 +233,3 @@ class Stock_Data():
 
 
 
-
-
-
-		
