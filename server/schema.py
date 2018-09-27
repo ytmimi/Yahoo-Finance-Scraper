07fe37c76aa34f8f6e1196a0b6126a8ae5e6410c
Yacin Tmimi <yacintmimi@gmail.com>
2018-09-27 16:13:19 -0400

    graphql schem and description files

---
+++ server/schema.py
@@ -0,0 +1,359 @@
+import json
+import os
+import sys
+from datetime import date, timedelta
+from functools import wraps
+
+BASE_PATH = os.path.abspath('.')
+#appends the yahoo_finance package to the path
+sys.path.append(os.path.join(BASE_PATH, 'yahoo_finance'))
+
+from yahoo_data import Stock_Data, Option_Data
+from utc_converter import date_str_from_timestamp as strftimestamp
+from schema_description import (
+	Stock_Price_Description as sp_desc,
+	Option_Description as op_desc,
+	Option_Chain_Description as opc_desc,
+	Ticker_Description as tik_desc,
+	Query_Description as q_desc,
+)
+
+import graphene
+
+stock = Stock_Data()
+option = Option_Data()
+
+def convert_timstamp(f):
+	'''Decorator that converst timstamps to strings'''
+	@wraps(f)
+	def wrapper(*args, **kwargs):
+		return strftimestamp(f(*args, **kwargs))
+	return wrapper
+
+def round_value(f, decimals=2):
+	'''Decorator that rounds Floats'''
+	@wraps(f)
+	def wrapper(*args, **kwargs):
+		return round(f(*args, **kwargs), decimals)
+	return wrapper
+
+#noramlly resolvers take in root(self) and info. but since I don't use
+#info, all custom resolvers dont take info as an argument
+@round_value
+def round_resolver(root, attr):
+	return getattr(root, attr)
+
+@convert_timstamp
+def date_resolver(root, date):
+	return getattr(root, date)
+
+class StockPrice(graphene.ObjectType):
+	date = graphene.String(
+		description=sp_desc.desc('date'),
+		resolver=lambda root, info: date_resolver(root, 'date')
+			)
+	open_ = graphene.Float(
+		name='open', description=sp_desc.desc('open_'),
+		resolver= lambda root, info: round_resolver(root, 'open_')
+		)
+	high = graphene.Float(
+		description=sp_desc.desc('high'),
+		resolver= lambda root, info: round_resolver(root, 'high')
+		)
+	low = graphene.Float(
+		description=sp_desc.desc('low'),
+		resolver= lambda root, info: round_resolver(root, 'low')
+		)
+	close = graphene.Float(
+		description=sp_desc.desc('close'),
+		resolver= lambda root, info: round_resolver(root, 'close')
+		)
+	volume = graphene.Float(
+		description=sp_desc.desc('volume'),
+		resolver= lambda root, info: round_resolver(root, 'volume')
+		)
+	adjclose = graphene.Float(
+		description=sp_desc.desc('adjclose'),
+		resolver= lambda root, info: round_resolver(root, 'adjclose')
+		)
+
+
+def raw_resolver(root, attr):
+	'''Some atributes are stored as dicts. When they are resolved
+		we want to access their raw data'''
+	return getattr(root, attr)['raw']
+
+@convert_timstamp
+def raw_exp_resolver(root, exp_date):
+	return getattr(root, exp_date)['raw']
+
+@round_value
+def raw_round_resolver(root, attr):
+	return getattr(root, attr)['raw']
+
+
+class Option(graphene.ObjectType):
+	contractSymbol = graphene.String(
+		description= op_desc.desc('contractSymbol'),
+		)
+	expiration = graphene.String(
+		description=op_desc.desc('expiration'),
+		resolver=lambda root, info: raw_exp_resolver(root, 'expiration')
+		)
+	change = graphene.Float(
+		description = op_desc.desc('change'),
+		resolver = lambda root, info: raw_resolver(root, 'change'),
+		)
+	lastPrice = graphene.Float(
+		description = op_desc.desc('lastPrice'),
+		resolver = lambda root, info: raw_resolver(root, 'lastPrice'),
+		)
+	inTheMoney = graphene.Boolean(
+		description = op_desc.desc('inTheMoney'),
+		)
+	impliedVolatility = graphene.Float(
+		description = op_desc.desc('impliedVolatility'),
+		resolver = lambda root, info: raw_round_resolver(root, 'impliedVolatility'),
+		)
+	openInterest = graphene.Float(
+		description = op_desc.desc('openInterest'),
+		resolver = lambda root, info: raw_resolver(root, 'openInterest'),
+		)
+	percentChange = graphene.Float(
+		description = op_desc.desc('percentChange'),
+		resolver = lambda root, info: raw_resolver(root, 'percentChange'),
+		)
+	bid = graphene.Float(
+		description = op_desc.desc('bid'),
+		resolver = lambda root, info: raw_resolver(root, 'bid'),
+		)
+	ask = graphene.Float(
+		description = op_desc.desc('ask'),
+		resolver = lambda root, info: raw_resolver(root, 'ask'),
+		)
+	volume = graphene.Float(
+		description = op_desc.desc('volume'),
+		resolver = lambda root, info: raw_resolver(root, 'volume'),
+		)
+	lastTradeDate = graphene.String(
+		description = op_desc.desc('lastTradeDate'),
+		resolver = lambda root, info: raw_exp_resolver(root, 'lastTradeDate'),
+		)
+	currency = graphene.String(
+		description = op_desc.desc('currency'),
+		)
+	strike = graphene.Float(
+		description = op_desc.desc('strike'),
+		resolver = lambda root, info: raw_resolver(root, 'strike'),
+		)
+	contractSize = graphene.String(
+		description = op_desc.desc('contractSize'),
+		)
+
+
+def map_options(option_data):
+	'''maps given option_data into an Option object'''
+	return list( map( lambda data: Option(**data), option_data))
+
+class OptionChain(graphene.ObjectType):
+	underlying = graphene.String(
+		required=True, description=opc_desc.desc('underlying')
+		)
+	date = graphene.String(
+		required=True, description=opc_desc.desc('expiration'),
+		resolver=lambda root, info: date_resolver(root, 'date')
+		)
+	strikes = graphene.List(
+		graphene.String, description=opc_desc.desc('strikes'),
+		)
+	calls = graphene.List(
+		Option, description=opc_desc.desc('calls'),
+		)
+	puts  = graphene.List(
+		Option, description=opc_desc.desc('puts'),
+		)
+	allOptions = graphene.List(
+		Option, description=opc_desc.desc('allOptions')
+		)
+
+	def resolve_strikes(self, info):
+		return option.strike_prices(self.underlying, self.date)
+
+	def resolve_calls(self, info):
+		call_data = option.calls(self.underlying, self.date)
+		return map_options(call_data)
+
+	def resolve_puts(self, info):
+		put_data = option.puts(self.underlying, self.date)
+		return map_options(put_data)
+
+	def resolve_allOptions(self, info):
+		return self.resolve_calls(info) +  self.resolve_puts(info)
+
+
+def default_start():
+	return (date.today()-timedelta(days=365)
+		).strftime('%m/%d/%y')
+
+def default_end():
+	return date.today().strftime('%m/%d/%y')
+
+def map_optionChain(ticker, exp_list):
+	'''maps each expiration date to  an OptionChain object
+	each OptionChain instance has its own methods to resolve its feilds
+	'''
+	return list(
+		map(lambda date: OptionChain(underlying=ticker, date=date), exp_list)
+		)
+
+def mutate_stock_data(data):
+	'''changing the data becuase open is reseved in Python '''
+	#need to make a copy not to affect the dictionary stored in the cache
+	new_data = data.copy()
+	new_data['open_'] = new_data.pop('open')
+	return new_data
+
+def map_stockPrice(stock_data):
+	'''maps stock_stock data to a stockPrice object'''
+	return list(map(lambda data: StockPrice(**mutate_stock_data(data)), stock_data))
+
+class Ticker(graphene.ObjectType):
+	ticker = graphene.String(
+		required=True, description=tik_desc.desc('ticker')
+		)
+	startDate = graphene.NonNull(
+		graphene.String, default_value=default_start(),
+		description=tik_desc.desc('startDate')
+		)
+	endDate = graphene.NonNull(
+		graphene.String, default_value=default_end(),
+		description=tik_desc.desc('endDate')
+		)
+	frequency = graphene.String(
+		default_value='1d', description=tik_desc.desc('frequency')
+		)
+	stockPrices = graphene.List(
+		StockPrice, description=tik_desc.desc('stockPrices')
+		)
+	calls = graphene.List(
+		Option, description=tik_desc.desc('calls')
+		)
+	puts = graphene.List(
+		Option, description=tik_desc.desc('puts')
+		)
+	optionChains = graphene.List(
+		OptionChain, limit = graphene.Int(
+		required=False, description= tik_desc.arg_desc('limit')
+		),
+		description = tik_desc.desc('optionChains')
+		)
+
+	def resolve_stockPrices(self, info):
+		stock_data = stock.stock_data(**{
+		'ticker':self.ticker, 'start_date':self.startDate,
+		'end_date':self.endDate, 'frequency':self.frequency,
+		})
+		return map_stockPrice(stock_data)
+
+	def resolve_calls(self, info):
+		import pdb; pdb.set_trace()
+		call_data = option.calls(self.ticker)
+		return map_options(call_data)
+
+	def resolve_puts(self, info):
+		put_data = option.puts(self.ticker)
+		return map_options(put_data)
+
+	def resolve_optionChains(self, info, **kwargs):
+		limit = kwargs.get('limit')
+		expirations = option.expirations(self.ticker)
+		if limit and limit > 0 and limit < len(expirations):
+			expirations = expirations[:limit]
+		return map_optionChain(self.ticker, expirations)
+
+
+
+def map_Tickers(ticker_list, **optional_args):
+	'''maps a list of stock tickers to Ticker objects'''
+	return list(map(lambda ticker: Ticker(ticker=ticker, **optional_args), ticker_list))
+
+
+def underlying_expiration(**kwargs):
+	return (kwargs.get('underlying'), kwargs.get('expiration'))
+
+#self does not work in the base Query object. Not 100% sure why
+class Query(graphene.ObjectType):
+	ticker = graphene.Field(
+		Ticker,
+		ticker=graphene.String(required=True, description=q_desc.arg_desc('ticker')),
+		startDate=graphene.String(description=q_desc.arg_desc('startDate')),
+		endDate=graphene.String(description=q_desc.arg_desc('endDate')),
+		frequency=graphene.String(description=q_desc.arg_desc('frequency')),
+		description=q_desc.desc('ticker')
+		)
+	tickers = graphene.List(
+		Ticker,
+		tickers=graphene.List(graphene.String, description=q_desc.arg_desc('tickers')),
+		startDate=graphene.String(description=q_desc.arg_desc('startDate')),
+		endDate=graphene.String(description=q_desc.arg_desc('endDate')),
+		frequency=graphene.String(description=q_desc.arg_desc('frequency')),
+		description=q_desc.desc('tickers')
+		)
+	calls = graphene.List(
+		Option,
+		underlying=graphene.String(required=True, description=q_desc.arg_desc('underlying')),
+		expiration=graphene.String(description=q_desc.arg_desc('expiration')),
+		description=q_desc.desc('calls')
+		)
+	puts = graphene.List(
+		Option,
+		underlying=graphene.String(required=True, description=q_desc.arg_desc('underlying')),
+		expiration=graphene.String(description=q_desc.arg_desc('expiration')),
+		description=q_desc.desc('puts')
+		)
+
+	optionStrikes = graphene.List(
+		graphene.Float,
+		underlying=graphene.String(required=True, description=q_desc.arg_desc('underlying')),
+		expiration=graphene.String(description=q_desc.arg_desc('expiration')),
+		description=q_desc.desc('strikes')
+		)
+
+	optionExpirationDates = graphene.List(
+		graphene.String,
+		underlying=graphene.String(required=True, description=q_desc.arg_desc('underlying')),
+		description=q_desc.desc('exp_dates')
+		)
+
+
+	def resolve_ticker(self, info, **kwargs):
+		'''kwargs: ticker, startDate, endDate, frequency'''
+		return Ticker(**kwargs)
+
+	def resolve_tickers(self, info, **kwargs):
+		'''kwargs: ticker, startDate, endDate, frequency'''
+		all_args = kwargs
+		tickers = all_args.pop('tickers')
+		return map_Tickers(tickers, **all_args)
+
+	def resolve_calls(self, info, **kwargs):
+		ticker,  _ = underlying_expiration(**kwargs)
+		call_data = option.calls(ticker)
+		return map_options(call_data)
+
+	def resolve_puts(self, info, **kwargs):
+		ticker, _ = underlying_expiration(**kwargs)
+		put_data = option.puts(ticker)
+		return map_options(put_data)
+
+	def resolve_optionStrikes(self, info, **kwargs):
+		ticker, _ = underlying_expiration(**kwargs)
+		return option.strike_prices(ticker)
+
+	def resolve_optionExpirationDates(self, info, **kwargs):
+		ticker, _ = underlying_expiration(**kwargs)
+		expirations = option.expirations(ticker)
+		return list(map(lambda data: strftimestamp(data), expirations))
+
+
+schema = graphene.Schema(query=Query)
---
+++ server/schema_description.py
@@ -0,0 +1,122 @@
+'''
+To avoid clutteing schema.py with descriptions, they have all been defined in this file.
+'''
+#constants that apear in multiple places
+TICKER = 'A valid stock ticker.'
+UNDERLYING = 'The ticker of the underlying stock.'
+OPTION_EXPIRATION = 'The expiration date of the option contract.'
+
+
+class Description:
+	'''
+	A Description class should define a description for all the fields used
+	in a corresponding graphQL(graphene) ObjectType. If certain fields require additional arguments,
+	each argument should be included as a class atribute of an associaated Meta class.
+	'''
+	@classmethod
+	def desc(cls, attr):
+		return getattr(cls, attr)
+
+	@classmethod
+	def arg_desc(cls, arg):
+		return getattr(cls.Meta, arg)
+
+class Stock_Price_Description(Description):
+	date = 'The date when the stock traded.'
+	open_ = 'The share price when the market opened.'
+	high = 'The highest share price on the given day.'
+	low = 'The lowest share price on the given day.'
+	close = 'The share price when the market closed.'
+	volume = 'The total amount of shares traded during the day.'
+	adjclose = 'The close price adjusted for dividends and stock splits.'
+
+
+class Option_Description(Description):
+	contractSymbol = 'A unique identifier giving information on the type, expiration, and strike of an option contract.'
+	expiration = OPTION_EXPIRATION
+	change = 'Decimal value representing the change in price.'
+	lastPrice = 'The last price at which a contract was bought and sold.'
+	inTheMoney = 'Defines whether the option contract has intrinsic value.'
+	impliedVolatility = 'A measure of the markets expectation of price movements in the underlying asset prior to the option\'s expiration.'
+	openInterest = 'The number of open contracts for the underlying at the given strike'
+	percentChange = 'Percentage representing the change in price.'
+	bid = 'The price offered to buy the option.'
+	ask = 'The price offered to sell the option.'
+	volume = 'the total number of contracts for each strike that traded during the day.'
+	lastTradeDate = 'Date showing the last time the contract was traded.'
+	currency =  'The currency the option contract trades in.'
+	strike = 'The price at which the underlying asset can be bought or sold.'
+	contractSize = 'The units of underlying that is represented by one contract. Typically one option contract covers 100 shares of stock.'
+	optionType = 'Indecates whether the option is a call or a put.'
+
+
+class Option_Chain_Description(Description):
+	underlying = UNDERLYING
+	expiration = OPTION_EXPIRATION
+	strikes = 'A list of all available stike prices for the given expiration date.'
+	calls = 'A list of all call options for the given expiration date.'
+	puts = 'A list of all put options for the given expiration date.'
+	allOptions = 'A list of all call and put options for the given expiration date.'
+
+
+
+
+class Ticker_Description(Description):
+	ticker = TICKER
+	startDate = 'The first date requested for stock data.'
+	endDate = 'The last date requested for stock data.'
+	frequency = 'Determins the frequency of stock price data. 1d for daily, 1wk for weekly, and 1mo for monthly '
+	stockPrices = 'A list of stock price data.'
+	calls = 'A list of near term call options for the given stock'
+	puts = 'A list of near term put options for the given stock.'
+	optionChains = 'A list of all option contracts that expire on the same day.'
+
+	class Meta:
+		limit = 'Limit the number of results returned. Must be > 0.'
+
+
+class Query_Description(Description):
+	ticker = (
+		'Data for a single stock.'
+		)
+
+	tickers = (
+		'Data for a list of stocks.'
+		)
+
+	calls = (
+		'Data on all call options for the given (optional) expiration date will be returned. '
+		'By default calls from the current expiration month will be returned.'
+		'If the data provided is not an exact expiration date the closest expiration following the '
+		'provided date with be returned. If there aren\'t any options expiring after the provided date, '
+		'the query will resolve to its default behavior.'
+		)
+
+	puts = (
+		'Data on all put options for the given (optional) expiration date will be returned. '
+		'By default puts from the current expiration month will be returned.'
+		'If the data provided is not an exact expiration date the closest expiration following the '
+		'provided date with be returned. If there aren\'t any options expiring after the provided date, '
+		'the query will resolve to its default behavior.'
+		)
+
+	strikes = (
+		'A list of all available strikes for the given expiration date will be returned.'
+		)
+
+	exp_dates = (
+		'A list of all currently available option expiration dates are return in mm/dd/yy format.'
+		)
+	class Meta:
+		ticker = TICKER
+		tickers = 'A list of valid stock ticker.'
+		startDate = 'A date in the form mm/dd/yyyy. Defaults to 365 days prior to the current date.'
+		endDate = 'A date in the form mm/dd/yyyy. Defaults to the current date.'
+		frequency = 'A given frequency for stock data. Values must be one of the following: 1d,  1wk, or, 1mo.'
+		underlying = UNDERLYING
+		expiration = 'The expiration date of the option contract in mm/dd/yyyy format. Defauts to the closest expiration.'
+
+
+
+if __name__ == '__main__':
+	print(Ticker_Description.arg_description('limit'))
