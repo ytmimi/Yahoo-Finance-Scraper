aafc49e392ce33a1fe35098b80319038ab456190
Yacin Tmimi <yacintmimi@gmail.com>
2018-10-01 19:43:52 -0400

    added new tests and made tests more DRY

--- test/test_yahoo_data.py
+++ test/test_yahoo_data.py
@@ -39,6 +39,29 @@ def option_mock_factory():
 	return factory
 
 
+STOCK_DATA_FIELDS = ['date', 'open', 'high', 'low', 'close', 
+	'volume', 'adjclose']
+
+RELATED_STOCK_FIELDS = ['symbol', 'openToday','highToday','lowToday',
+	'closePrevious', 'recentPrice', 'volumeToday','sharesOtstanding',
+	'marketCap','fiftyTwoWeekHigh', 'fiftyTwoWeekLow','fiftyTwoWeekRange',]
+
+OPTION_DATA_FIELDS = ['contractSymbol','impliedVolatility','expiration',
+	'change','currency','strike','contractSize','lastPrice','inTheMoney',
+	'openInterest','percentChange','ask','volume','lastTradeDate','bid']
+
+
+def assert_dictionary_list(lst, keys):
+	'''lst: list containing dict items. keys: list of expected keys'''
+	for dictionary in lst:
+		assert isinstance(dictionary, dict)
+		assert list(dictionary.keys()) == keys
+
+def assert_number_list(lst):
+	for num in lst:
+		assert isinstance(num, float) or isinstance(num, int)
+
+
 class Test_Mock_Stock_Data:
 	def test_stock_data_return_list(self, stock_mock_factory):
 		stock = stock_mock_factory(settings.STOCK_DATA_1D)
@@ -48,11 +71,32 @@ class Test_Mock_Stock_Data:
 	def test_stock_data_values(self, stock_mock_factory):
 		stock = stock_mock_factory(settings.STOCK_DATA_1D)
 		data = stock.stock_data('JWN')
-		for dictionary in data:
-			assert isinstance(dictionary, dict)
-			assert list(dictionary.keys()) == ['date', 'open', 
-					'high', 'low', 'close', 'volume', 'adjclose']
+		assert_dictionary_list(data, STOCK_DATA_FIELDS)
 
+	def test_related_tickers_return_list(self, stock_mock_factory):
+		stock = stock_mock_factory(settings.STOCK_DATA_1D)
+		data = stock.relate_tickers('JWN')
+		assert isinstance(data, list)
+
+	def test_related_tickers_values(self, stock_mock_factory):
+		stock = stock_mock_factory(settings.STOCK_DATA_1D)
+		data = stock.relate_tickers('JWN')
+		assert_dictionary_list(data, RELATED_STOCK_FIELDS)
+
+	def test_overview_return_list(self, stock_mock_factory):
+		stock = stock_mock_factory(settings.STOCK_DATA_1D)
+		data = stock.overview('JWN')
+		assert isinstance(data, list)
+
+	def test_overview_default(self, stock_mock_factory):
+		stock = stock_mock_factory(settings.STOCK_DATA_1D)
+		data = stock.overview('JWN')
+		assert_dictionary_list(data, RELATED_STOCK_FIELDS)
+
+	def test_overview_related_tickers(self, stock_mock_factory):
+		stock = stock_mock_factory(settings.STOCK_DATA_1D)
+		data = stock.overview('JWN', relate_tickers='True')
+		assert_dictionary_list(data, RELATED_STOCK_FIELDS)
 
 
 class Test_Mock_Option_Data:
@@ -60,8 +104,7 @@ class Test_Mock_Option_Data:
 		option = option_mock_factory(settings.OPTION_DATA_1)
 		data = option.strike_prices('JWN')
 		assert isinstance(data, list)
-		for num in data:
-			assert isinstance(num, float) or isinstance(num, int)
+		assert_number_list(data)
 
 	def test_option_expirations(self, option_mock_factory):
 		option = option_mock_factory(settings.OPTION_DATA_1)
@@ -71,20 +114,12 @@ class Test_Mock_Option_Data:
 	def test_option_calls(self, option_mock_factory):
 		option = option_mock_factory(settings.OPTION_DATA_1)
 		data = option.calls('JWN')
-		for dictionary in data:
-			assert isinstance(dictionary, dict)
-			assert list(dictionary.keys()) == ['contractSymbol','impliedVolatility','expiration',
-			'change','currency','strike','contractSize','lastPrice','inTheMoney',
-			'openInterest','percentChange','ask','volume','lastTradeDate','bid']
+		assert_dictionary_list(data, OPTION_DATA_FIELDS)
 
 	def test_option_puts(self, option_mock_factory):
 		option = option_mock_factory(settings.OPTION_DATA_1)
 		data = option.puts('JWN')
-		for dictionary in data:
-			assert isinstance(dictionary, dict)
-			assert list(dictionary.keys()) == ['contractSymbol','impliedVolatility','expiration',
-			'change','currency','strike','contractSize','lastPrice','inTheMoney',
-			'openInterest','percentChange','ask','volume','lastTradeDate','bid']
+		assert_dictionary_list(data, OPTION_DATA_FIELDS)
 
 
 @pytest.mark.skipif(not settings.LIVE_TEST, 
@@ -97,10 +132,7 @@ class Test_Live_Stock_Data:
 
 	def test_stock_data_values(self):
 		data = self.stock.stock_data('JWN')
-		for item in data:
-			assert isinstance(item, dict)
-			assert list(item.keys()) == ['date', 'open', 
-					'high', 'low', 'close', 'volume', 'adjclose']
+		assert_dictionary_list(data, STOCK_DATA_FIELDS)
 
 
 @pytest.mark.skipif(not settings.LIVE_TEST, 
@@ -110,8 +142,7 @@ class Test_Live_Option_Data:
 	def test_option_strike_prices(self):
 		data = self.option.strike_prices('JWN')
 		assert isinstance(data, list)
-		for num in data:
-			assert isinstance(num, float) or isinstance(num, int)
+		assert_number_list(data)
 
 	def test_option_expirations(self):
 		data = self.option.expirations('JWN')
@@ -119,20 +150,11 @@ class Test_Live_Option_Data:
 
 	def test_option_calls(self):
 		data = self.option.calls('JWN')
-		for dictionary in data:
-			assert isinstance(dictionary, dict)
-			assert list(dictionary.keys()) == ['contractSymbol','impliedVolatility','expiration',
-			'change','currency','strike','contractSize','lastPrice','inTheMoney',
-			'openInterest','percentChange','ask','volume','lastTradeDate','bid']
+		assert_dictionary_list(data, OPTION_DATA_FIELDS)
 
 	def test_option_puts(self):
 		data = self.option.puts('JWN')
-		for dictionary in data:
-			assert isinstance(dictionary, dict)
-			assert list(dictionary.keys()) == ['contractSymbol','impliedVolatility','expiration',
-			'change','currency','strike','contractSize','lastPrice','inTheMoney',
-			'openInterest','percentChange','ask','volume','lastTradeDate','bid']
-
+		assert_dictionary_list(data, OPTION_DATA_FIELDS)
 
 
 
