58f739bf34746ac50faeabf68802463b77ce6b2e
Yacin Tmimi <yacintmimi@gmail.com>
2018-10-01 16:56:59 -0400

    updated to create smaller mock data files

--- test/generate_mock_data.py
+++ test/generate_mock_data.py
@@ -6,10 +6,12 @@ import sys
 import json
 from functools import wraps
 
-BASE_PATH = os.path.abspath('.')
-sys.path.append(os.path.join(BASE_PATH, 'yahoo_finance'))
-import yahoo_data as yd
-import yahoo_finance as yf
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_PATH)
+
+import yahoo_finance.yahoo_data as yd
+import yahoo_finance.yahoo_finance as yf
+import settings
 
 stock_scraper = yf.Stock_Scraper
 option_scraper = yf.Options_Scraper
@@ -19,15 +21,18 @@ option = yd.Option_Data()
 
 
 def stock_scraper_kwargs(ticker, frequency):	
-	return {'ticker':ticker, 'frequency':frequency, 
-			'start_date':'09/26/2018', 'end_date':'09/26/2017'}
+	return {
+			'ticker':ticker, 
+			'frequency':frequency, 
+			'start_date':settings.MOCK_DATA_START, 
+			'end_date':settings.MOCK_DATA_END
+			}
 
 def option_scraper_kwargs(ticker, expiration):
 	return {'ticker':ticker, 'expiration_date':expiration}
 
-def make_directory(name):
+def make_directory(path):
 	#creates a new directory if it doesn't exist
-	path = os.path.join(BASE_PATH, name)
 	os.makedirs(path, exist_ok=True)
 	return path
 
@@ -48,21 +53,29 @@ def run_function(base_scraper, data_generator, function, *func_args, **func_kwar
 	new_scraper = base_scraper(**func_kwargs)
 	#executes the function. Uses data_generator in place of self
 	function(*func_args, **func_kwargs)
+	#used to get the key so that we can acces the data_generators cache
 	url = new_scraper._process_url()
 	key = data_generator._data_key(url)
-	#returns the data stored that was just collected from yahoo finance
-	return data_generator.cache[key]
+	#get the data stored in the cache that was just collected from yahoo finance
+	cached = data_generator.cache[key]
+	##pop unesessary data out of the cache copy
+	important_data = cached.copy()
+	for key in cached.keys():
+		if key not in ['HistoricalPriceStore','QuoteSummaryStore' 
+		'RecommendationStore', 'StreamDataStore', 'OptionContractsStore']:
+			important_data.pop(key)
+	return important_data
 
 
 
 if __name__ == '__main__':
-	data_directory = make_directory('mock_data')
+	data_directory = make_directory(settings.MOCK_PATH)
 	ticker = 'JWN'
 	frequencies = ['1d', '1wk', '1mo']
 	for frequency in frequencies:
 		kwargs = stock_scraper_kwargs(ticker, frequency)
 		data = json.dumps(run_function(stock_scraper, stock, 
-						stock.stock_data, **kwargs), indent=4)	
+						stock.stock_data, **kwargs), indent=4) 
 		write_to_json(data_directory, f'{ticker}_{frequency}.json', data)
 
 	exp_months = option.expirations(ticker)
